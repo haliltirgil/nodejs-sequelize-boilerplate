4cb57b3fa94960fd8e7c1662d4f52fd00847192f
haliltirgil <tirgil18@gmail.com>
2022-10-29 15:29:03 +0300

    Add route index structure.

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+require("dotenv/config");
+const express = require("express");
+const testRouter = require("./test");
+
+const router = express.Router();
+
+router.use('/test', testRouter);
+
+module.exports = router;
