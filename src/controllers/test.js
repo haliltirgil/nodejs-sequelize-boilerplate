b93911221b54e29de611e915fdf53c3c5411cba6
haliltirgil <tirgil18@gmail.com>
2022-11-13 15:18:01 +0300

    Delete.

--- src/controllers/test.js
+++
@@ -1,9 +0,0 @@
-exports.createDevice = async (_req, res) => {
-    try {
-        return res.status(200).json({
-            message: "Test endpoint!"
-        });
-    } catch (error) {
-
-    }
-};
--- src/routes/test.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const testController = require("../controllers/test");
-
-const router = express.Router();
-
-router.get('/get', testController.createDevice);
-
-module.exports = router; 
