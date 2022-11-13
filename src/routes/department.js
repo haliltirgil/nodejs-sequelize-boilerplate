f953a1ac3e909d6dcc72de3a9b51f392ac32d7ed
haliltirgil <tirgil18@gmail.com>
2022-11-13 16:20:54 +0300

    Add routes for endpoints.

---
+++ src/routes/department.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { createDepartment, getAllDepartments, getDepartmentById, updateDepartmentById, deleteDepartmentById } = require("../controllers/department");
+
+const router = express.Router();
+
+router.post('/create-department', createDepartment);
+router.get('/get-all-departments', getAllDepartments);
+router.get('/get-department-by-id/:id', getDepartmentById);
+router.patch('/update-department-by-id/:id', updateDepartmentById);
+router.delete('/delete-department-by-id/:id', deleteDepartmentById);
+
+module.exports = router; 
---
+++ src/routes/project.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { createProject, getAllProjects, getProjectById, updateProjectById, deleteProjectById } = require("../controllers/project");
+
+const router = express.Router();
+
+router.post('/create-project', createProject);
+router.get('/get-all-projects', getAllProjects);
+router.get('/get-project-by-id/:id', getProjectById);
+router.patch('/update-project-by-id/:id', updateProjectById);
+router.delete('/delete-project-by-id/:id', deleteProjectById);
+
+module.exports = router; 
