a2bccf67e9c808a184138bc1468294c9200a34a7
haliltirgil <tirgil18@gmail.com>
2022-12-18 17:34:59 +0300

    Add database connection service.

---
+++ src/services/database.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+
+const sequelize =
+  process.env.NODE_ENV === 'production'
+    ? new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      logging: false,
+    })
+    : new Sequelize(process.env.TEST_DB_NAME, process.env.TEST_DB_USER, process.env.TEST_DB_PASS, {
+      dialect: 'postgres',
+      host: process.env.TEST_DB_HOST,
+      logging: false
+    });
+
+module.exports = sequelize;
