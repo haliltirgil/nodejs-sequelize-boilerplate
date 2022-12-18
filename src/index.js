5b6e1525300e5b3049104189043428222bb739e3
haliltirgil <tirgil18@gmail.com>
2022-12-18 17:33:31 +0300

    Edit database connection method.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const app = require('./app');
+const sequelize = require('./services/database');
+
+const databaseConnection = async () => {
+    try {
+        await sequelize.sync();
+        console.log('Database connection successful.');
+    } catch (error) {
+        console.log(error);
+        setTimeout(databaseConnection, 3000);
+    }
+};
 
 const start = async () => {
+    await databaseConnection();
+
     app.listen(process.env.SERVER_PORT, () => {
         console.log(`Server started on port ${process.env.SERVER_PORT}`);
-        console.log(process.env.TEST);
     })
 }
 
